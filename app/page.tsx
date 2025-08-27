10a7bf43ef3b33039155e85e54f5abcb3090f82e
Alekissandro <alek.rossi@gmail.com>
2025-08-27 10:29:13 -0300

    inicio

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <>
+    <div className="font-sans flex flex-col min-h-screen p-8 gap-3 sm:p-20">
+      <p className="text-lg"> Hello, my name is</p>
+      <h1 className="text-4xl sm:text-4xl font-bold text-center">Alekissandro Rossi Viana de Oliveira</h1>
+      <h1 className="text-xl sm:text-xl font-bold text-center">Software DEV, Eletronic Engineer and Eletronic Technician</h1>
+      <p className="text-lg">
+        I am self-taught Frontend developer, that started to develop by curiosity and has been hooked by this profession for more than 10 years. I have been studying Javascript, Python and Dart. 
+        Lately I have been working developing apps and websites for small business.
+        </p>
+        <p className="text-lg">
+        I have been working for The Brazilian Air Force for 22 years as a eletronic technician, which is one of the most important military institutions in Brazil. I have dealt with people from different backgrounds people from Italy, Canada, United States, France and so on... I also have developed as a professional because for many times I had to find out the crash in the system in order to solve it, and come up with solutions to make our work better. I have developed small programs to facilitate our day-to-day, and seen the results helping people in my work environment. 
+        I am ready for new challenges.
+        </p>
+        <p className="text-lg">
+        I became a christian in 1999, and after sometime I became a leader, working as a mentor in their christian life, giving them advice, guidance and support. In 2014 I became a Bible sunday school teacher and have been teaching the Bible and helping people to find their purpose in life.
+        </p>
+        <p className="text-lg">
+        I was blessed with a wonderful family, my wife, who is a dedicated mother and a fantastic partner for dreaming, living, planning and building a great home for us, and our three kids who came to show us how to really love.
+        </p>
+        <h1 className="text-4xl sm:text-2xl font-bold text-left">SKILLS</h1>
+        <p className="text-lg">
+        HTML, CSS, JavaScript, TypeScript, React, Next.js, TailwindCSS.
+        </p>
+        <p className="text-lg">
+        VSCS systems, VHF systems, Satellite systems.
+        </p>  
+         <h1 className="text-4xl sm:text-2xl font-bold text-center">CONTACT</h1>      
+        <p className="text-lg text-center">
+        email: alek.rossi@gmail.com
+        </p>
+        <p className="text-lg text-center">
+        cell phone: +55 41 99703-9657
+      </p>
     </div>
+    </>
   );
 }
